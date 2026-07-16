a7fe124eaa975c18d44af48498e5f4ed823e379b
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-07-16 09:42:55 +0700

    chore(style): setup tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import vue from '@vitejs/plugin-vue';
+import Tailwindcss from '@tailwindcss/vite';
+import Vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        // https://github.com/vuejs/core
+        Vue(),
+
+        // https://github.com/tailwindlabs/tailwindcss
+        Tailwindcss(),
+    ],
 });
