85d507da552b7cc54c0b3fc081ae5e4ab299e9dc
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-07-16 10:06:07 +0700

    chore(vite): setup vite config (env mode, alias, server port)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './core/styles/index.css';
+import '@core/styles/index.css';
 
 createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,39 @@
+import path from 'node:path';
 import Tailwindcss from '@tailwindcss/vite';
 import Vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [
-        // https://github.com/vuejs/core
-        Vue(),
+// // https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+    const env = {
+        ...process.env,
+        ...loadEnv(mode, `${process.cwd()}/environments`, ''),
+    };
+    const port = !Number.isNaN(Number(env.VITE_APP_DEV_PORT))
+        ? Number(env.VITE_APP_DEV_PORT)
+        : 8002;
 
-        // https://github.com/tailwindlabs/tailwindcss
-        Tailwindcss(),
-    ],
+    return {
+        plugins: [
+            // https://github.com/vuejs/core
+            Vue(),
+
+            // https://github.com/tailwindlabs/tailwindcss
+            Tailwindcss(),
+        ],
+
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@root': path.resolve(__dirname),
+                '@core': path.resolve(__dirname, 'src/core'),
+                '@modules': path.resolve(__dirname, 'src/modules'),
+            },
+        },
+
+        server: {
+            port,
+            strictPort: true,
+        },
+    };
 });
