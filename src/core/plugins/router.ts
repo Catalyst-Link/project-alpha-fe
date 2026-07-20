2e81d179615189554e0b375fad1f5d749cbb42c2
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-07-20 10:53:44 +0700

    chore(router): setup nprogress config

--- src/core/plugins/router.ts
+++ src/core/plugins/router.ts
@@ -1,4 +1,5 @@
 import type { App } from 'vue';
+import NProgress from 'nprogress';
 import { createRouter, createWebHistory } from 'vue-router';
 import { handleHotUpdate, routes } from 'vue-router/auto-routes';
 
@@ -11,6 +12,15 @@ export function install(app: App<Element>) {
     if (import.meta.hot) {
         handleHotUpdate(router);
     }
+
+    router.beforeEach(() => {
+        window.scrollTo(0, 0);
+        NProgress.start();
+    });
+
+    router.afterEach(() => {
+        NProgress.done();
+    });
 
     app.use(router);
 }
