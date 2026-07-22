c32433b53805ad98143164c220547705048dd653
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-07-22 20:28:23 +0700

    feat(component): wIP: add VInput component

---
+++ src/core/components/base/VInput/VInput.vue
@@ -0,0 +1,24 @@
+<!-- TODO: Setup Variants & Styling the component -->
+
+<script lang="ts" setup>
+import type { Props } from '.';
+
+const props = defineProps<Props>();
+const { value, errorMessage } = useField(toRef(props, 'name'), undefined, {
+    initialValue: props.modelValue,
+    validateOnMount: false,
+    syncVModel: true,
+});
+</script>
+
+<template>
+    <div class="input-wrapper">
+        <input v-model="value">
+        <span
+            v-if="errorMessage"
+            class="error"
+        >
+            {{ errorMessage }}
+        </span>
+    </div>
+</template>
