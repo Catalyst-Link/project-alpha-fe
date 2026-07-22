fc5d665f0c81e6457df447f5928dab54347f6d94
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-07-22 11:34:46 +0700

    chore(env): add key base api url into env

--- src/core/utils/EnvironmentUtil.ts
+++ src/core/utils/EnvironmentUtil.ts
@@ -1,16 +1,7 @@
 export const env = object({
-    VITE_APP_DEV_MODE: string()
-        .oneOf(['development', 'staging', 'production'])
-        .required(),
-
-    VITE_APP_DEV_PORT: string()
-        .transform((value) => {
-            const parsed = Number(value);
-            return Number.isNaN(parsed)
-                ? undefined
-                : parsed;
-        })
-        .required(),
+    VITE_APP_DEV_MODE: string().oneOf(['development', 'staging', 'production']).required(),
+    VITE_APP_DEV_PORT: string().required(),
+    VITE_BASE_API_URL: string().required(),
 }).validateSync(import.meta.env, {
     stripUnknown: true,
 });
