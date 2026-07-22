bda9b2d9769eeaa891dad210d1aaa9560ac22a97
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-07-22 20:11:01 +0700

    refactor(util): remove environment util

--- src/core/utils/EnvironmentUtil.ts
+++
@@ -1,7 +0,0 @@
-export const env = object({
-    VITE_APP_DEV_MODE: string().oneOf(['development', 'staging', 'production']).required(),
-    VITE_APP_DEV_PORT: string().required(),
-    VITE_BASE_API_URL: string().required(),
-}).validateSync(import.meta.env, {
-    stripUnknown: true,
-});
