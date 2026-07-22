0aa06eab46a7a6953b59f29205fa6a33a7e62290
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-07-22 10:32:08 +0700

    chore(di): setup di config

---
+++ src/core/exceptions/HttpResponseError.ts
@@ -0,0 +1,8 @@
+export class HttpResponseError extends Error {
+    type!: ErrorType;
+
+    constructor({ message, type = 'RESPONSE_ERROR' }: ErrorValues) {
+        super(message);
+        this.type = type;
+    }
+}
