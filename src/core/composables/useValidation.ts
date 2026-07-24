a94f021c3cd429acb0abfb6240ac37b239bcb700
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-07-24 09:44:26 +0700

    refactor(composable): translate from bahasa to eng

--- src/core/composables/useValidation.ts
+++ src/core/composables/useValidation.ts
@@ -7,7 +7,7 @@ export function useValidation() {
         function validatePhoneNumber() {
             return this.test({
                 message({ label }: { label: string }) {
-                    return `${label} harus diawali 08 dan memiliki 10-13 digit.`;
+                    return `${label} must start with 08 and have 10-13 digits.`;
                 },
                 test: value => /^08\d{8,11}$/.test(String(value)),
             });
@@ -16,17 +16,17 @@ export function useValidation() {
 
     setLocale({
         mixed: {
-            required: ({ label }: { label: string }) => `Silahkan isi ${label} terlebih dahulu.`,
-            notType: <T>({ label, type }: { label: string; type: T }) => `Kolom ${label} harus bertipe ${type}.`,
+            required: ({ label }: { label: string }) => `Please fill in ${label}.`,
+            notType: <T>({ label, type }: { label: string; type: T }) => `The ${label} field must be of type ${type}.`,
         },
         number: {
-            min: ({ label, min }: { label: string; min: number }) => `${label} harus diisi dengan minimal ${min} karakter.`,
-            max: ({ label, max }: { label: string; max: number }) => `${label} harus diisi dengan maksimal ${max} karakter.`,
+            min: ({ label, min }: { label: string; min: number }) => `${label} must be at least ${min}.`,
+            max: ({ label, max }: { label: string; max: number }) => `${label} must be at most ${max}.`,
         },
         string: {
-            email: ({ label }: { label: string }) => `${label} harus berupa alamat email yang valid.`,
-            min: ({ label, min }: { label: string; min: number }) => `${label} harus diisi dengan minimal ${min} karakter.`,
-            max: ({ label, max }: { label: string; max: number }) => `${label} harus diisi dengan maksimal ${max} karakter.`,
+            email: ({ label }: { label: string }) => `${label} must be a valid email address.`,
+            min: ({ label, min }: { label: string; min: number }) => `${label} must be at least ${min} characters.`,
+            max: ({ label, max }: { label: string; max: number }) => `${label} must be at most ${max} characters.`,
         },
     });
 }
