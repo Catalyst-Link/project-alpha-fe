ea323b120b0256fb55cd9ddaaadf58f4566e4676
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2026-07-27 21:13:04 +0700

    feat(constant): add sidebar constant

--- src/core/constants/CommonConstant.ts
+++ src/core/constants/CommonConstant.ts
@@ -1,3 +1,10 @@
+export const SIDEBAR_COOKIE_NAME = 'sidebar_state';
+export const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
+export const SIDEBAR_WIDTH = '16rem';
+export const SIDEBAR_WIDTH_MOBILE = '18rem';
+export const SIDEBAR_WIDTH_ICON = '3rem';
+export const SIDEBAR_KEYBOARD_SHORTCUT = 'b';
+
 export const ENDPOINT_PATH = '/catalyst-link-up' as const;
 export const ENDPOINT_VERSION = {
     V1: 'v1',
